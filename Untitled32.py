072548d336eed8e74db89e74d607ad7684565268
Jamil79 <71585338+Jamil79@users.noreply.github.com>
2020-09-23 08:34:58 +0300

    Add files via upload

---
+++ Untitled32.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+data = pd.read_csv('C:/Users/96132/Desktop/titanic.csv')
+data.head()
+
+
+# In[ ]:
+
+
+
+
